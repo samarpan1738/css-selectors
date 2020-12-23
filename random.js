6490cfaddf65a3225ecd5096900f2b1a5296e952
Samarpan Harit <haritsamarpan7@gmail.com>
2020-12-23 21:32:37 +0530

    Initial commit

---
+++ random.js
@@ -0,0 +1,8 @@
+// Publication Date 
+document.querySelectorAll(".lbl-licitacao font")[3].innerText
+// Bidding Date
+document.querySelectorAll(".lbl-licitacao font")[18].innerText
+// Object
+document.querySelectorAll(".views-field-nothing>.field-content>p")[0].innerText
+// All links related to this particular tender
+document.querySelectorAll(`#page-main-content a[href^="http"]`)
